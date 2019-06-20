810340fe8a95dea6f4360018906151119e8cefd6
James Scharett <jdscharett@gmail.com>
2019-06-20 22:47:38 +0200

    test: modeled test for jsonloader

--- projects/demo/src/app/json-loader.service.spec.ts
+++ projects/demo/src/app/json-loader.service.spec.ts
@@ -1,6 +1,8 @@
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
+import { cold } from 'jasmine-marbles';
+
 import { JsonLoaderService } from './json-loader.service';
 
 describe('JsonLoaderService', () => {
@@ -16,5 +18,15 @@ describe('JsonLoaderService', () => {
     it('should be created', () => {
         const service: JsonLoaderService = TestBed.get(JsonLoaderService);
         expect(service).toBeTruthy();
+    });
+
+    it('should have observable examples', () => {
+        const service: JsonLoaderService = TestBed.get(JsonLoaderService);
+        expect(service.examples).toBeObservable(cold('-'));
+    });
+
+    it('should have observable example', () => {
+        const service: JsonLoaderService = TestBed.get(JsonLoaderService);
+        expect(service.getExample('ngx', 'basic')).toBeObservable(cold('-'));
     });
 });
