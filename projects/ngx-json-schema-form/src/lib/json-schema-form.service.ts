d8f2ee1af0ebdf1958d238585c1f3d84d986589a
James Scharett <jdscharett@gmail.com>
2019-07-30 20:47:14 +0200

    docs: Added missing docs
    Also fixed some lint issues

--- projects/ngx-json-schema-form/src/lib/json-schema-form.service.ts
+++ projects/ngx-json-schema-form/src/lib/json-schema-form.service.ts
@@ -12,7 +12,33 @@ import { Widget } from './widget-library';
 export class JsonSchemaFormService {
     private x = false;
 
-    constructor(private readonly compiler: Compiler, private readonly injector: Injector, private moduleRef: NgModuleRef<any>) {}
+    /** Creates a component with passed in template and associated context */
+    private static createComponent(template: string, context: {[key: string]: any}): any {
+        return Component({
+            selector: `runtime-component-sample`,
+            template
+        })(class RuntimeComponent {
+            options: any = context;
+        });
+    }
+
+    /** Create a module for use with the RuntimeComponent */
+    private static createModule(component: any, compiler: Compiler): ModuleWithComponentFactories<any> {
+        @NgModule({ imports: [CommonModule], declarations: [component] })
+        class RuntimeComponentModule { }
+
+        return compiler.compileModuleAndAllComponentsSync(RuntimeComponentModule);
+    }
+
+    /** Get a components innerHTML as a document Fragment */
+    private static getDocumentFragment(componentRef: ComponentRef<any>): DocumentFragment {
+        const template = document.createElement('template');
+        template.innerHTML = (<HTMLElement>componentRef.location.nativeElement).innerHTML;
+
+        return template.content;
+    }
+
+    constructor(private readonly compiler: Compiler, private readonly injector: Injector, private readonly moduleRef: NgModuleRef<any>) {}
 
     /** Sets a widgets properties upon Widget creation */
     initializeControl(control: Widget, bind = true): void {
@@ -26,55 +52,21 @@ export class JsonSchemaFormService {
         this.x = !this.x;
     }
 
-    // Take a look ad Dynamic component Loading
-    // https://angular.io/guide/dynamic-component-loader
-    // Think about adding an ng-template to a container component
-    // The component can then dynamically create a component with user template
-    // and have it injected into the template.  The component can wrap the user
-    // template in a template itself, and then publically expose it on a prop.
-    // The conainter then "should" have access to the template and be able
-    // to pass that inject that.  Would allow for ijecting the whole component
-    // and letting angular control it, however, will need to look out
-    // for memory leaks and such.
-    // <button><ng-container *ngTemplateOutlet="custom; context:options"></ng-container></button>
-    // <ng-template>
-    //    <custom-component>
-    //         <ng-template #custom>
-    //             ...user content...
-    compileTemplate(template: string, context: {[key: string]: any}): DocumentFragment {
-        const component: any = this.createComponent(template, context);
-        const module = this.createModule(component);
+    /**
+     * Compiles an HTML tempalte and data into a document fragment
+     * @param template - HTML template to render
+     * @param context - options to use when rendering the template
+     */
+    compileTemplate(template: string, context: {[key: string]: any} = {}): DocumentFragment {
+        const component: any = JsonSchemaFormService.createComponent(template, context);
+        const module = JsonSchemaFormService.createModule(component, this.compiler);
         const factory = module.componentFactories.find((f) => f.componentType === component);
         const componentRef: ComponentRef<any> = factory.create(this.injector, undefined, undefined, this.moduleRef);
 
         componentRef.hostView.detectChanges();
-        const fragment = this.getDocumentFragment(componentRef);
+        const fragment = JsonSchemaFormService.getDocumentFragment(componentRef);
         componentRef.destroy();
 
         return fragment;
     }
-
-    createComponent(template: string, context: {[key: string]: any}): any {
-        return Component({
-            selector: `runtime-component-sample`,
-            template
-        })(class RuntimeComponent {
-            options: any = context;
-        });
-    }
-
-    createModule(component: any): ModuleWithComponentFactories<any> {
-        @NgModule({ imports: [CommonModule], declarations: [component] })
-        class RuntimeComponentModule { }
-
-        return this.compiler.compileModuleAndAllComponentsSync(RuntimeComponentModule);
-    }
-
-    getDocumentFragment(componentRef: ComponentRef<any>): DocumentFragment {
-        const template = document.createElement('template');
-        template.innerHTML = (<HTMLElement>componentRef.location.nativeElement).innerHTML;
-
-        return template.content;
-    }
-
 }
