ccbbdd3357e3768f496f353540ade4e256362e75
James Scharett <7661580+jscharett@users.noreply.github.com>
2020-07-06 13:53:58 +0200

    refactor: removed unused class

--- projects/ngx-json-schema-form/src/lib/textarea/textarea.component.ts
+++ projects/ngx-json-schema-form/src/lib/textarea/textarea.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { Widget } from '../core/decorators/widget.decorator';
 import { ElementDataStorageService } from '../core/services/element-data-storage.service';
